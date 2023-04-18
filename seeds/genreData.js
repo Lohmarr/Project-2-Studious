453e6af5a5b991c9924c28df361f697c1a38d912
Jimmys Mac <eadesj25@gmail.com>
2023-04-18 12:01:59 -0400

    seed messages and genre data

--- seeds/genreData.js
+++ seeds/genreData.js
@@ -3,18 +3,31 @@ const { Genre } = require('../models');
 const genreData = [
   {
     name: 'Rock',
+    image_name: 'rockgenre.jpg',
   },
   {
     name: 'Rap',
+    image_name: 'rapgenre.jpg',
   },
   {
     name: 'Country',
+    image_name: 'countrygenre.jpg',
   },
   {
     name: 'Pop',
+    image_name: 'popgenre.jpg',
   },
   {
-    name: 'Indie'
+    name: 'Indie',
+    image_name: 'indiegenre.jpg',
+  },
+  {
+    name: 'Funk',
+    image_name: 'funkgenre.jpg',
+  },
+  {
+    name: 'Electronic',
+    image_name: 'electronic.jpg',
   }
 ];
 
